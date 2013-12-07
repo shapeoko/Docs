3e5ff9367766a8c6fd8c0aa7ed5826688b010820
Edward <edward@shapeoko.com>
2013-12-07 12:22:45 -0600

    changed converter format
    eliminated h2 from header

--- content/doc_converter.php
+++ content/doc_converter.php
@@ -3,7 +3,7 @@
 	include('Parsedown.php');
 	$getHeader = file_get_contents('header.html');
 	$getFooter = file_get_contents('footer.html');
-	$articleClose = '</article>';
+	
 	
 
 
@@ -11,26 +11,22 @@
 	    //Get the contents of the markdown file into a string
 	    $content = file_get_contents("$file");
 	    
-	    //Article name based off file name.
-	    //$articleTitle = substr(substr($file, 0, -3), 2);
+	    //Article name based of file name.
 	    $articleTitle = basename($file, ".md");
-	    $subHeader =  '<h2 id="introText">' . $articleTitle . '</h2>';
 
 	    //create new file from markdown file name
 	    $newfile = $articleTitle . '.html';
 		$handle = fopen($newfile, 'w') or die('Cannot open file:  '. $newfile); //implicitly creates file
 
-		//Open the article tag
+		//define the article tags
 		$articleOpen = '<article id="'.$articleTitle.'">';
-		
+		$articleClose = '</article>';
+
 		//Parse the markdown into HTML
 		$sectionContent= Parsedown::instance()->parse($content);
 
-		//spit the whole thing out
-		//$current .= $articleOpen . $sectionContent . $articleClose;
-
 		//generate page for each md
-		$newpage = $getHeader . $subHeader . $articleOpen . $sectionContent . $articleClose . $getFooter;
+		$newpage = $getHeader . $articleOpen . $sectionContent . $articleClose . $getFooter;
 		file_put_contents($newfile, $newpage);
 	    }
 
--- content/tips.md
+++ content/tips.md
@@ -1,3 +1,5 @@
+#Pro Tips!
+
 ###Screw Sizes (lengths)
 
 
